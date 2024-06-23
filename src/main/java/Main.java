40f90e18b2e9f5e0158886ca15df6c03314c4eb0
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-06-23 16:08:04 +0200

    s

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        print("master");
     }
     private static void print(String m){
         System.out.println("branch v2" + m);
