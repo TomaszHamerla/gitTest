2276bf80da879b2fec870179f25ce4d1da304029
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-06-23 15:54:26 +0200

    '

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("master");
         print("master");
+        print2("master");
     }
     static void print(String m){
         System.out.println(m);
+    }
+    private static void print2(String m){
+        System.out.println("print2 method: " + m);
     }
 }
