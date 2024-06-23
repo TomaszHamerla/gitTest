21e813af8d2809f590f7dd9d2b2c870dd67072b7
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-06-23 15:49:22 +0200

    init v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("master");
+        print("master");
+    }
+    static void print(String m){
+        System.out.println(m);
     }
 }
